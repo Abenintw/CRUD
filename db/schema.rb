1a1841e3d2082095de0fa4eef81be46e50c7afd0
Bens <bensanuj@gmail.com>
2012-05-05 11:22:46 +0800

    firstmodify

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505022326) do
+ActiveRecord::Schema.define(:version => 20120505031242) do
 
   create_table "posts", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "title"
+    t.text     "content"
   end
 
 end
